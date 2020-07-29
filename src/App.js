886a776911c90c49cf87941d5482620e4f77044e
KoTuK <dmitit_aleshin_00@mail.ru>
2020-07-29 15:09:26 +0300

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header/header';
 import Navbar from './components/Navbar/navbar';
 import Profile from './components/Profile/profile';
-import Description from './components/ProfileIformation/Description/description';
 
  const App = () => {
   return (
