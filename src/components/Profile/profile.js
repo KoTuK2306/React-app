6d6107834096621adc513047aae359ad2ec8dd4f
KoTuK <dmitit_aleshin_00@mail.ru>
2020-07-29 15:29:05 +0300

    The file hierarchy has been improved

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './profile.css';
-import ProfileBackground from './../ProfileBackground/profile-background'
-import Description from './../ProfileIformation/Description/description'
-import NewPost from '../NewPost/new-post';
-import MyPosts from '../MyPosts/my-posts';
+import ProfileBackground from './../Profile/ProfileBackground/profile-background'
+import Description from './../Profile/ProfileIformation/Description/description'
+import NewPost from '../Profile/NewPost/new-post';
+import MyPosts from '../Profile/MyPosts/my-posts';
 
 const Profile = () =>{
     return (
