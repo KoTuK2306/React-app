10ee2dee228fe7b435de3b3f4b4288f06dced949
KoTuK <dmitit_aleshin_00@mail.ru>
2020-07-31 14:26:30 +0300

    Added Dialogs component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import classes from './App.module.css';
 import Header from './components/Header/header';
 import Navbar from './components/Navbar/navbar';
 import Profile from './components/Profile/profile';
+import Dialogs from './components/Dialogs/dialogs';
 
-const App = () => {
+const App = (props) => {
   return (
     <div className = {classes.content}>
-      
+
       <Header />
       <Navbar />
-      <Profile />
+      <div className = {classes.contentItem}>
+
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
       
+    
+
     </div>
   );
 }
-
 
 export default App;
