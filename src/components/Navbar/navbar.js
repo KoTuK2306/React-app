16edcaf16c1ff472672d01174053dee9e0452fb5
KoTuK <dmitit_aleshin_00@mail.ru>
2020-07-31 17:08:21 +0300

    Was added better NavLink's

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,11 +5,35 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () =>{
     return(
         <nav className = {classes.nav}>
-            <div className = {classes.navText}><NavLink to = '/profile'>Profile</NavLink></div>
-            <div className = {classes.navText}><NavLink to = '/dialogs'>Messages</NavLink></div>
-            <div className = {classes.navText}><NavLink to = '/news'>News</NavLink></div>
-            <div className = {classes.navText}><NavLink to = '/music'>Music</NavLink></div>
-            <div className = {classes.navText}><NavLink to = '/settings'>Settings</NavLink></div>
+            <div className = {classes.navText}>
+                <NavLink to = '/profile' activeClassName = {classes.active}>
+                    Profile
+                </NavLink>
+            </div>
+
+            <div className = {classes.navText}>
+                <NavLink to = '/dialogs' activeClassName = {classes.active}>
+                    Messages
+                </NavLink>
+            </div>
+
+            <div className = {classes.navText}>
+                <NavLink to = '/news' activeClassName = {classes.active}>
+                    News
+                </NavLink>
+            </div>
+
+            <div className = {classes.navText}>
+                <NavLink to = '/music' activeClassName = {classes.active}>
+                    Music
+                </NavLink>
+            </div>
+
+            <div className = {classes.navText}>
+                <NavLink to = '/settings' activeClassName = {classes.active}>
+                    Settings
+                </NavLink>
+            </div>
         </nav>
     );
 }
