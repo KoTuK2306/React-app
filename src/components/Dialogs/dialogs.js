94b64ce068f5fc282f6761c1d6ac39f133571159
KoTuK <dmitit_aleshin_00@mail.ru>
2020-07-31 20:38:53 +0300

    Was added dialogs page and first steps of her layout

--- src/components/Dialogs/dialogs.js
+++ src/components/Dialogs/dialogs.js
@@ -4,7 +4,40 @@ import classes from './dialogs.module.css';
 const Dialogs = (props) =>{
     return(
         <div className = {classes.dialogs}>
-            Dialogs
+            
+            <div className = {classes.dialogsItems}>
+
+                <div className = {classes.dialogsItem + ' ' + classes.active}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+                <div className = {classes.dialogsItem}>
+                    Zhopa
+                </div>
+            </div>  
+
+            <div className = {classes.messages}>
+
+                <div className = {classes.message}>Hi</div>
+                <div className = {classes.message}>Bye</div>
+                <div className = {classes.message}>Have a good day!</div>
+
+            </div>
+
         </div>
     );
 }
