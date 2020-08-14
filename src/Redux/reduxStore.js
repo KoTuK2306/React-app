24ee10f41cc29a59cca5d9e17909f196d9271112
KoTuK <dmitit_aleshin_00@mail.ru>
2020-08-14 12:33:32 +0300

    Was intalled react-redux

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -2,12 +2,10 @@ import {combineReducers, createStore} from 'redux';
 import profileReducer from './profileReducer';
 import messagesReducer from './messagesReducer';
 
-
 let reducers = combineReducers({
     messagesPage: messagesReducer,
     profilePage: profileReducer
 });
-
 
 let store = createStore(reducers);
 
