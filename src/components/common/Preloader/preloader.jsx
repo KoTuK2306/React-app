8ee7c258684e06bdfbfc6f26d77f26b72d2f32ae
KoTuK <dmitit_aleshin_00@mail.ru>
2020-08-21 18:04:25 +0300

    Deleted preloader svg and was replace on just css code

--- src/components/common/Preloader/preloader.jsx
+++ src/components/common/Preloader/preloader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import preloader from '../../../assets/images/preloader.svg';
 import classes from './preloader.module.css';
 
 const Preloader = () =>{
     return(
-        <div className={classes.img}>
-            <img className={classes.imgItem} alt='' src={preloader}/>
+        <div className={classes.preloaderSection}>
+            <div className={classes.preloader}></div>
         </div>
+        
     );
 }
 
