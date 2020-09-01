8a4391c41c485ea4183996be36a6e2cfca21d894
KoTuK <dmitit_aleshin_00@mail.ru>
2020-09-01 11:24:29 +0300

    changed styles in navbar and dialog component

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -6,38 +6,38 @@ const Navbar = (props) =>{
     const isOpen = props.isOpenNavbar ? classes.open : '';
     return(
         <nav className = {`${classes.nav} ${isOpen}`}>
-            <div className = {classes.navText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/profile' activeClassName = {classes.active}>
+            <div className = {classes.navItem}>
+                <NavLink className = {classes.navText} onClick={() => props.closeNavbar(false)} to = '/profile' activeClassName = {classes.active}>
                     Profile
                 </NavLink>
             </div>
 
-            <div className = {classes.navText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/dialogs' activeClassName = {classes.active}>
+            <div className = {classes.navItem}>
+                <NavLink className = {classes.navText} onClick={() => props.closeNavbar(false)} to = '/dialogs' activeClassName = {classes.active}>
                     Messages
                 </NavLink>
             </div>
 
-            <div className = {classes.navText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/news' activeClassName = {classes.active}>
+            <div className = {classes.navItem}>
+                <NavLink className = {classes.navText} onClick={() => props.closeNavbar(false)} to = '/news' activeClassName = {classes.active}>
                     News
                 </NavLink>
             </div>
 
-            <div className = {classes.navText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/music' activeClassName = {classes.active}>
+            <div className = {classes.navItem}>
+                <NavLink className = {classes.navText} onClick={() => props.closeNavbar(false)} to = '/music' activeClassName = {classes.active}>
                     Music
                 </NavLink>
             </div>
 
-            <div className = {classes.anotherNavText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/users' activeClassName = {classes.active}>
+            <div className = {classes.anotherNavItem}>
+                <NavLink className = {classes.anotherNavText} onClick={() => props.closeNavbar(false)} to = '/users' activeClassName = {classes.active}>
                     Find Users
                 </NavLink>
             </div>
 
-            <div className = {classes.anotherNavText}>
-                <NavLink onClick={() => props.closeNavbar(false)} to = '/settings' activeClassName = {classes.active}>
+            <div className = {classes.anotherNavItem}>
+                <NavLink  className = {classes.anotherNavText} onClick={() => props.closeNavbar(false)} to = '/settings' activeClassName = {classes.active}>
                     Settings
                 </NavLink>
             </div>
